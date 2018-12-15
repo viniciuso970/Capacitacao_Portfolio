b45779bfafbf51905a47d293991d869b50d66a63
Vinicius Oliveira <vinicius.oliveira@justdigital.com.br>
2018-12-15 17:15:04 -0200

    InícioProjeto/Atualização Wordpress/Requisitos 3 e 4

---
+++ wp-content/themes/capacitacao/assets/js/scripts.js
@@ -0,0 +1,9 @@
+(function($) {
+    $('.galeria').slick({
+        infinite: true,
+        slidesToShow: 1,
+        autoPlay: true,
+        autoplaySpeed: 1000,
+        arrows: true
+    });
+}(jQuery))
--- wp-content/themes/capacitacao/header.php
+++ wp-content/themes/capacitacao/header.php
@@ -8,5 +8,30 @@
     <?php wp_head();?>
 </head>
 <body>
-    <header>
+    <header>      
+        <nav>
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+            </div>
+        </nav>
+        <div class="galeria">
+            <?php
+                $args = array( 'post_type' => 'galeria', 'posts_per_page' => 10 );
+                $loop = new WP_Query( $args );
+                while ( $loop->have_posts() ) : $loop->the_post();
+            ?>
+            <div>
+                <img src="<?php echo the_field('imagem');?>" alt="Galeria" srcset="">
+                <?php echo the_content(); ?>
+            </div>
+            
+            <?php
+                endwhile;
+            ?>
+        </div>
     </header>
