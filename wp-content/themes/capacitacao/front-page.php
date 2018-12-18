5ac1593afd9a687ba3a82f110b3ab033cef22803
Vinicius Oliveira <vinicius.oliveira@justdigital.com.br>
2018-12-18 20:38:36 -0200

    Desenvolvendo Galeria

--- wp-content/themes/capacitacao/front-page.php
+++ wp-content/themes/capacitacao/front-page.php
@@ -107,7 +107,7 @@
         ?>
     </div>
     <?php
-        $args = array( 'post_type' => 'galeria', 'posts_per_page' => 6 );
+        $args = array( 'post_type' => 'galeria', 'posts_per_page' => 7 );
         $loop = new WP_Query( $args );
         $nro_imagens = $loop->found_posts;
         $class_name = "";
@@ -115,30 +115,26 @@
             $class_name = "grid-s1-m1";
         } else if($nro_imagens == 2) {
             $class_name = "grid-s1-m2";
-        } else if($nro_imagens == 3) {
-            $class_name = "galeria-grid-3";
-        } else if($nro_imagens == 4) {
-            $class_name = "galeria-grid-4";
-        } else if($nro_imagens == 5) {
-            $class_name = "galeria-grid-5";
+        } else if($nro_imagens != 7) {
+            $class_name = "grid-s1-m2-l3";
         } else {
-            $class_name = "galeria-grid-6";
+            $class_name = "galeria-grid-7";
         }
     ?>
     <div class="imagem-grid <?php echo $class_name; ?>">
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
         <div class="galeria">
-            <a class="fluidbox">
-                <?php echo the_post_thumbnail(); ?>
-            </a>
-            <div class="galeria-text hide">
-                <h1><?php echo the_title(); ?></h1>
-                <?php
-                    $categorias = get_the_category();
-                    foreach($categorias as $category) {
-                        echo '<p><a href="'. esc_url( get_category_link( $category->term_id)) .'">'. get_cat_name( $category->term_id) .'</a></p>';
-                    }
-                ?>
+            <div class="imagens">
+                <?php echo get_the_post_thumbnail(); ?>
+                <div class="galeria-text hide">
+                    <h1><?php echo the_title(); ?></h1>
+                    <?php
+                        $categorias = get_the_category();
+                        foreach($categorias as $category) {
+                            echo '<p><a href="'. esc_url( get_category_link( $category->term_id)) .'">'. get_cat_name( $category->term_id) .'</a></p>';
+                        }
+                    ?>
+                </div>
             </div>
         </div>
         <?php
